bec73f4277bbb0c7652302dc7f0aed40d441131f
pawannraj <98880956+pawannraj@users.noreply.github.com>
2022-03-07 09:48:28 +0530

    Update activity2.c

--- project/3_Implementation/Src/activity2.c
+++ project/3_Implementation/Src/activity2.c
@@ -6,6 +6,13 @@ void Init_ADC()
     ADCSRA=(1<<ADEN)|(7<<ADPS0);
 }
 
+/**
+ * @brief A funciton to receive temperature sensor inputs from analog pin and output it to a register ADC which has 2 8 bit registers ADCL and ADCH.
+ * 
+ * @param ch 
+ * @return uint16_t 
+ */
+
 uint16_t Read_ADC(uint8_t ch)
 {
     ADMUX&=0xf8;
