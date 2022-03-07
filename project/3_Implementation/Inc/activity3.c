2ac1b09d4ff79b20c8f7377731922506b628cff1
pawannraj <98880956+pawannraj@users.noreply.github.com>
2022-03-07 09:46:32 +0530

    Update activity3.c

--- project/3_Implementation/Inc/activity3.c
+++ project/3_Implementation/Inc/activity3.c
@@ -1,1 +1,21 @@
+#ifndef __ACTIVITY_3_H__
+#define __ACTIVITY_3_H__
 
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+/**
+ * @brief A function to setup timer1 with channel A for pulse width modulation with wave generation mode of PWM.
+ * 
+ */
+void TimerWave(void);
+
+/**
+ * @brief A function which receives ADC input from temperature sensor and outputs as a PWM waveform.
+ * 
+ * @param ADC_val 
+ */
+void outputPWM(uint16_t ADC_val);
+
+#endif
