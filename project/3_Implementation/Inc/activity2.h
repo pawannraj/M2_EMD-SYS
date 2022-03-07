f9c8b06f77266af2d05ada0ac929407faad33445
pawannraj <98880956+pawannraj@users.noreply.github.com>
2022-03-07 09:45:48 +0530

    Update activity2.h

--- project/3_Implementation/Inc/activity2.h
+++ project/3_Implementation/Inc/activity2.h
@@ -1,1 +1,16 @@
+#ifndef __ACTIVITY_2_H__
+#define __ACTIVITY_2_H__
 
+#include <avr/io.h>
+
+#define SET_ADC_INTERRUPT_FLAG ADCSRA|=(1<<ADIF)
+
+#define ADC_INTERRUPT_NOT !(ADCSRA & (1<<ADIF))
+
+void Init_ADC();
+
+uint16_t Read_ADC(uint8_t ch);
+
+
+
+#endif
