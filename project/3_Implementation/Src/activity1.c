6b2f59ac55193e37b10360e4d2820de9856d6a0e
pawannraj <98880956+pawannraj@users.noreply.github.com>
2022-03-07 09:39:46 +0530

    Update activity1.c

--- project/3_Implementation/Src/activity1.c
+++ project/3_Implementation/Src/activity1.c
@@ -1,1 +1,29 @@
+#include "activity1.h"
 
+
+void delay_ms(uint32_t delay_time)
+{
+	uint32_t units = 0;
+	for (units = 0; units <= delay_time; units++)
+	{
+		_delay_ms(1);
+	}
+}
+void change_led_state(uint8_t state)
+{
+
+        if(PINC & (1<<0))
+        {
+            PORTB= PORTB | (1<<1);
+        }
+        else
+        {
+            PORTB= PORTB & ~(1<<1);
+        }
+}
+void peripheral_init(void)
+{
+	/* Configure LED Pin */
+	DDRB =DDRB | (1<<1);
+    DDRC =DDRC & ~(1<<0);
+}
