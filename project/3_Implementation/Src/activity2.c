cf5ccf4ba9bb12bf98f7331d81eee4f2176f0697
pawannraj <98880956+pawannraj@users.noreply.github.com>
2022-03-07 09:40:39 +0530

    Update activity2.c

--- project/3_Implementation/Src/activity2.c
+++ project/3_Implementation/Src/activity2.c
@@ -1,1 +1,21 @@
+#include "activity2.h"
+#include <avr/io.h>
+void Init_ADC()
+{
+    ADMUX=(1<<REFS0);
+    ADCSRA=(1<<ADEN)|(7<<ADPS0);
+}
 
+uint16_t Read_ADC(uint8_t ch)
+{
+    ADMUX&=0xf8;
+    ch = ch&0b00000111;
+    ADMUX|=ch;
+
+    ADCSRA|=(1<<ADSC);
+
+    while(ADC_INTERRUPT_NOT);
+    SET_ADC_INTERRUPT_FLAG;
+    return(ADC);
+
+}
