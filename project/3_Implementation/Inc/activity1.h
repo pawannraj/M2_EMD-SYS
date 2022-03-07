0fcfb4865f051b1aaebe214890b76182115bead0
pawannraj <98880956+pawannraj@users.noreply.github.com>
2022-03-07 09:44:39 +0530

    Update activity1.h

--- project/3_Implementation/Inc/activity1.h
+++ project/3_Implementation/Inc/activity1.h
@@ -1,1 +1,24 @@
+#ifndef ACTIVITY1_H_INCLUDED
+#define ACTIVITY1_H_INCLUDED
 
+#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+/**
+ * Function Definitions
+ */
+
+/**
+ * @brief To blink the led when a person occuipes seat and switch on the heater
+ *
+ * @param state Pin level to which the LED Pin should be set
+ */
+void change_led_state(uint8_t state);
+
+void delay_ms(uint32_t delay_time);
+void peripheral_init(void);
+
+
+#endif // ACTIVITY1_H_INCLUDED
