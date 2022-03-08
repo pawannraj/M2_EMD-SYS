dcc430ce5c45395fc6fd2bc50a6cc509ca20cab8
pawannraj <98880956+pawannraj@users.noreply.github.com>
2022-03-08 15:30:16 +0530

    Update main.c

--- project/3_Implementation/main.c
+++ project/3_Implementation/main.c
@@ -11,9 +11,9 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include "project1.h"
-#include "project2.h"
-#include "project3.h"
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
 
 
 int main(void)
